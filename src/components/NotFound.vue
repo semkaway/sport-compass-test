5a243708e3ac0ab95754e87f222bc107b46e43f5
semkaway <semkaway@gmail.com>
2019-02-28 12:13:58 +0200

    [ADD] 404

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+      <h1>404</h1>
+      <b-button variant="danger">Button</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from '@/components/MainPage'
-// import NotFound from '@/components/NotFound'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
@@ -14,14 +14,14 @@ let router = new Router({
 			name: 'home',
 			component: MainPage,
 		},
-		// {
-		// 	path: '/404',
-		// 	component: NotFound
-		// },
-		// {
-		// 	path: '*',
-		// 	redirect: '/404'
-		// }
+		{
+			path: '/404',
+			component: NotFound
+		},
+		{
+			path: '*',
+			redirect: '/404'
+		}
 	]
 })
 
